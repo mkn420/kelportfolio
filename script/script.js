e8f350b307a97a67f55d0ddfde30d787e29533e7
Doomed <111266615+mkn420@users.noreply.github.com>
2023-12-16 06:53:14 +0800

    Update script.js

--- script/script.js
+++ script/script.js
@@ -127,3 +127,16 @@ themeToggleBtn.addEventListener('click', function() {
     }
     
 });
+
+
+var options = {
+         animate: true,
+         patternWidth: 100,
+         patternHeight: 100,
+         grainOpacity: 0.05,
+         grainDensity: 1,
+         grainWidth: 1,
+         grainHeight: 1
+ };
+
+grained('#wrapper', options);
